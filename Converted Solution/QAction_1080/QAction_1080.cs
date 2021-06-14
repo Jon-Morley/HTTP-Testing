52765cf68c82ee77aa3a16078d6d10cbac9318c2
Jon Morley <Jon.Morley@dazn.com>
2021-06-14 10:53:50 +0100

    14/06/2021

--- Converted Solution/QAction_1080/QAction_1080.cs
+++ Converted Solution/QAction_1080/QAction_1080.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
-
+using Skyline.DataMiner.Library.Common;
+using Skyline.DataMiner.Net.Messages;
 using Skyline.DataMiner.Scripting;
+using Parameter = Skyline.DataMiner.Scripting.Parameter;
 
 /// <summary>
+/// 
 /// DataMiner QAction Class: Poll endpoints.
 /// </summary>
 public static class QAction
@@ -18,8 +21,20 @@ public static class QAction
 	{
 		try
 		{
+			List<EndpointsQActionRow> rowObjects = new List<EndpointsQActionRow>();
+
+			EndpointsQActionTable table = protocol.endpoints;
+			for(int i =0; i< table.RowCount; i++)
+			{
+				object[] rowData = table.GetRow(i);
+				EndpointsQActionRow nextRow = (EndpointsQActionRow)rowData;
+				rowObjects.Add(nextRow);
+			}
+
+
+
 			//debug
-			protocol.SetParameter(Parameter.http_debug_1081, "QAction 1080 ran at " + DateTime.Now.ToString());			
+			protocol.SetParameter(Parameter.http_debug_1081, "QAction 1080 ran at " + DateTime.Now.ToString() + " with " + rowObjects.Count.ToString() + "entries in the table");			
 		}
 		catch (Exception ex)
 		{
